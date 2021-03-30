10b3e1596ba98dcb02c6ecc60e6e98ff670ee514
tommyli3318 <tommyli3318@gmail.com>
2021-03-30 16:05:57 -0700

    fix comment

--- 3dmodel.py
+++ 3dmodel.py
@@ -1,16 +1,8 @@
 
 #https://forge.autodesk.com/en/docs/reality-capture/v1/tutorials/create-3d-mesh-from-photos/
 #https://forge.autodesk.com/blog/hitchhikers-guide-reality-capture-api?linkId=48983534#utm_source=Blog&utm_medium=Twitter&utm_term=RealityCapture&utm_content=TipsandTricks
+# create class that utilizes API
 class RenderModel:
-    #Hmm ok so what did scott say about the use of the api
-    #like did he say reference it and create something similar?
-    #Ohh that makes sense. Gotcha gotcha, so we just need to create a class that will call
-    #the API and then use that model 
-    
-    #I think when I asked about it he told us that we should have like a base mesh
-    #basically I think he meant that we make a basic mesh from this api and
-    #then add the details later on using tommy's depth and tejas stuff you know
-    #
     def __init__(self): 
         pass
 
