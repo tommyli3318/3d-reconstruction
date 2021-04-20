b6d1d3a5f0700b25cbb371835168611c95347d54
tommyli3318 <tommyli3318@gmail.com>
2021-04-19 17:44:43 -0700

    new progress, added new helper

--- generate-using-landmarks.py
+++ generate-using-landmarks.py
@@ -2,13 +2,25 @@ import pix2vertex as p2v
 from imageio import imread, imwrite
 from landmarks import recognize_image
 
-image_path = 'tommy-images/middle.jpg'
-image = imread(image_path)
-result, crop = p2v.reconstruct(image)
-front_depth_arr = result['Z_surface']
+def helper(image_path):
+    image = imread(image_path)
+    result, crop = p2v.reconstruct(image)
+    
+    cropped_path = 'temp/cropped.jpg'
+    imwrite(cropped_path, crop)
+    
+    landmarks = recognize_image(cropped_path, show_image=True)
+    
+    return result['Z_surface'], landmarks
 
-cropped_path = 'cropped.jpg'
-imwrite(cropped_path, crop)
+def main(front, left, right):
+    front_arr, front_landmarks = helper(front)
+    left_arr, left_landmarks = helper(left)
+    right_arr, right_landmarks = helper(right)
+    
+    # print(front)
+    # print(left)
+    # print(right)
 
-landmarks = recognize_image(cropped_path, show_image=False)
-print(landmarks)
+# main('tommy-images/middle.jpg', 'tommy-images/left.jpg', 'tommy-images/right.jpg')
+main('ray-images/ray-front.jpeg', 'ray-images/ray-left.jpeg', 'ray-images/ray-right.jpeg')
