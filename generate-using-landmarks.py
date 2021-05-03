1e3f45bd08f1ecfe3c1b69d3eea8cc3054602942
tommyli3318 <tommyli3318@gmail.com>
2021-05-03 13:57:06 -0700

    refactor

--- generate-using-landmarks.py
+++ generate-using-landmarks.py
@@ -13,12 +13,12 @@ def helper(image_path):
     
     landmarks = recognize_image(cropped_path, show_image=False)
     
-    return result['Z_surface'], landmarks
+    return result['Z_surface'], landmarks, crop
 
 
 def apply(src, src_landmarks, des, des_landmarks, landmarks_to_apply, r):
     # apply an area around each landmark from src image to des image
-
+    # src and des are both 2D depth arrays from pix2vert
     for i in landmarks_to_apply:
         src_y, src_x = src_landmarks[i]
         des_y, des_x = des_landmarks[i]
@@ -36,18 +36,22 @@ def apply(src, src_landmarks, des, des_landmarks, landmarks_to_apply, r):
                 if abs(np.linalg.norm([i,j])) > r:
                     continue
                 
-                des[c,d] = src[a,b]
+                des[c,d] = src[a,b] # implement average()
 
 
 def main(front_path, left_path, right_path):
-    front, front_landmarks = helper(front_path)
-    left, left_landmarks = helper(left_path) # use landmarks 0-7(jaw), 17-21(brow), 36-41(eye)
-    right, right_landmarks = helper(right_path) # use landmarks 9-16(jaw), 22-26(brow), 42-47(eye)
+    front, front_landmarks, front_crop = helper(front_path)
+    left, left_landmarks, _ = helper(left_path) # use landmarks 0-7(jaw), 17-21(brow), 36-41(eye)
+    right, right_landmarks, _ = helper(right_path) # use landmarks 9-16(jaw), 22-26(brow), 42-47(eye)
 
     apply(left, left_landmarks, front, front_landmarks, list(range(0,8))+list(range(17,22))+list(range(36,42)), 10)
     apply(right, right_landmarks, front, front_landmarks, list(range(9,17))+list(range(22,27))+list(range(42,48)), 10)
 
-    p2v.save2stl(front, 'ray.stl')
+
+    # visualization using matplotlib
+    # p2v.vis_depth_matplotlib(front_crop, front)
+
+    p2v.save2stl(front, 'out.stl')
     
     
 if __name__ == '__main__':
